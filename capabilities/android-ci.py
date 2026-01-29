c3004e20a65bf15d68da01a7bf189ceabc94424f
David Melchor <dmelchornatplat@gmail.com>
2026-01-28 22:13:46 -0600

    Creacion de archivo yaml

--- capabilities/android-ci.py
+++ capabilities/android-ci.py
@@ -5,16 +5,14 @@ APK_PATH = os.getenv("APK_PATH")
 if not APK_PATH:
     raise Exception("APK_PATH no está definido")
 
-desired_caps = {
-    "platformName": "Android",
-    "deviceName": "emulator-5554",
-    "automationName": "UiAutomator2",
-    "avd": "appium",
-    "app": "APK_PATH",
-    "appPackage": "",         # no usados en CI
-    "appActivity": "",
-    "newCommandTimeout": 300,
-    "adbExecTimeout": 60000,
-    "autoGrantPermissions": True,
-    "noReset": False
-}
+platformName = "Android",
+deviceName = "emulator-5554",
+automationName =  "UiAutomator2",
+avd = "appium",
+app = "APK_PATH",
+appPackage =  "",         # no usados en CI
+appActivity = "",
+newCommandTimeout = 300,
+adbExecTimeout = 60000,
+autoGrantPermissions = True,
+noReset = False
